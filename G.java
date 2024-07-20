ee858daef586b8583dbcaeb0fc4658da2910d82e
VINAY-SANDEEP <rsrinivasa782@gmail.com>
2024-07-20 17:29:50 +0530

    arraylistchange

--- G.java
+++ G.java
@@ -2,12 +2,11 @@ import java.util.*;
 public class G {
     public static void main(String[] args) {
         Vector v1 = new Vector();
+        v1.add("vishnu");
         v1.add("priya");
         v1.add("surekha");
-        v1.add("sudha");
-        v1.add("sandhya");
-        v1.add("sneha");
-        System.out.println(v1.get(4));
-        
+        v1.add("senha");
+        v1.add("yavru");
+        System.err.println(v1);
     }
 }
