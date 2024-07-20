be1a782afbcf4653154f2003514397065f601f9f
VINAY-SANDEEP <rsrinivasa782@gmail.com>
2024-07-20 17:20:37 +0530

    arrayslist

--- G.java
+++ G.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class G {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
+        Vector v1 = new Vector();
+        v1.add("priya");
+        v1.add("surekha");
+        v1.add("sudha");
+        v1.add("sandhya");
+        v1.add("sneha");
+        System.out.println(v1.get(4));
+        
     }
 }
